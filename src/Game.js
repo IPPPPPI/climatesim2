45417d669bcd2502341356226731b26d65033b4d
IPPPPPI <17991307xiao@gmail.com>
2023-03-27 11:07:13 -0700

    Push for text display and skill tree
    Co-authored-by: AndIFellInTheRiver <AndIFellInTheRiver@users.noreply.github.com>

--- src/Game.js
+++ src/Game.js
@@ -5,65 +5,71 @@ import "./App.css";
 //events for the games that will run down
 const events = {
   initial: {
-    description:
-      "Hello, I am your secertary. You are the newly elected mayor of Oaktown, a town who is in the middle of a climate disaster. Now you will be making decision for the next four years. Good Luck Mayor!",
-    yes: "gascars_question",
+    solar: "solar_invest",
+    nuclear: "nuclear_invest",
+    hydro: "hydro_invest",
+    wind: "wind_invest",
+    back: "go_back",
   },
-  gascars_question: {
-    description: "Do you want to ban Gas Car within the city limit?",
-    yes: "banned_cars",
-    no: "allowed_cars",
+  solar_invest: {
+    solar: "solar_invested",
   },
-  banned_cars: {
-    description:
-      "Okay, I am sure our car driver won't get mad right? It seem our major party donor had left. Not suprising. We got to save the environment first." +
-      " Anyway, next agenda in the list, Should we subsidize 'more tree in the city' petition? ",
+  nuclear_invest: {
+    nuclear: "nuclear_invested",
+  },
+  hydro_invest: {
+    hydro: "hydro_invested",
+  },
+  wind_invest: {
+    wind: "wind_invested",
+  },
+  solar_invested: {
     statsChanges: {
       money: -20000,
       happiness: 5,
       environment: 3,
     },
-    yes: "allow_trees",
-    no: "no_trees",
   },
-  allowed_cars: {
-    description:
-      "Good choice, I mean in what kind of person bans car the first thing in their mayor term? I mean that is asking for trouble." +
-      " Oh right I just checked the our government fund, someone just donated 100,000 dollar." +
-      " Next agenda on the list, we have recieved a subsidize 'more tree in the city' petition. It seem our party sponsor want us to reject the Bill.",
+  nuclear_invested: {
     statsChanges: {
-      money: 100000,
-      happiness: -2,
-      environment: -3,
-    },
-    yes: "allow_trees",
-    no: "no_trees",
-  },
-  allow_trees: {
-    description:
-      "Who doesn't like trees for shade. I am sure people are happy from this decision." +
-      "WOW, what was that shake? Oh it seem a nearby city was struck with a major earthquake. Luckily we were pretty far from the center." +
-      "It seem a report had came out that a nearby coal plants collapsed due to the earthquake. Should we use this opptunity to replace it with Solar?",
-    statsChanges: {
-      money: -1000,
+      money: -10000,
       happiness: 5,
-      environment: 5,
+      environment: 3,
     },
-    yes: "replace_coal",
-    no: "don't_replace_coal",
   },
-  no_trees: {
-    description:
-      "Really? I mean we have to save money somehow I guess." +
-      "WOW, what was that shake? Oh it seem a nearby city was struck with a major earthquake. Luckily we were pretty far from the center." +
-      "It seem a report had came out that a nearby coal plants collapsed due to the earthquake. Should we use this opptunity to replace it with Solar?",
+  hydro_invested: {
     statsChanges: {
-      happiness: -20,
+      money: 20000,
+      happiness: 5,
+      environment: 3,
     },
-    yes: "replace_coal",
-    no: "don't_replace_coal",
+  },
+  wind_invested: {
+    statsChanges: {
+      money: 40000,
+      happiness: 5,
+      environment: 3,
+    },
   },
 };
+
+/*
+const nodes = {
+  initial: {
+
+  },
+  initial1: {
+    parent: "initial",
+    cost: {
+      money: 20
+    }
+  },
+  initial2: {
+    parent: "initial",
+    cost: {}
+  }
+}
+*/
 
 //checks the events above to see what valiue it need to change (Special thanks to Mr.Pig)
 const useStore = create((set, get) => ({
@@ -92,30 +98,49 @@ export default function Game() {
   const eventData = events[event];
   return (
     <>
-      <p>{eventData.description}</p>
       <br></br>
       <p>Resources</p>
       <p>Happiness level: {happiness}</p>
       <p>Money: ${money}</p>
       <p>Population: {population} people</p>
       <p>Environment: {environment} level</p>
-
-      {eventData.yes && (
+      {eventData.solar && (
         <button
           className="button1"
           type="button"
-          onClick={() => setEvent(eventData.yes)}
+          onClick={() => setEvent(eventData.solar)}
         >
-          Yes
+          Solar Power
+          <div className="solarTTB">
+            <span className="solarTT">Money -200</span>
+          </div>
         </button>
       )}
-      {eventData.no && (
+      {eventData.nuclear && (
         <button
           className="button2"
           type="button"
-          onClick={() => setEvent(eventData.no)}
+          onClick={() => setEvent(eventData.nuclear)}
         >
-          No
+          Wind power
+        </button>
+      )}
+      {eventData.hydro && (
+        <button
+          className="button3"
+          type="button"
+          onClick={() => setEvent(eventData.hydro)}
+        >
+          Hydro power
+        </button>
+      )}
+      {eventData.wind && (
+        <button
+          className="button4"
+          type="button"
+          onClick={() => setEvent(eventData.wind)}
+        >
+          Wind power
         </button>
       )}
     </>
