bf95567dc5c16540ec14ec1aae6db3979e93f4b8
IPPPPPI <17991307xiao@gmail.com>
2023-04-12 11:07:58 -0700

    still have a lot of work to do
    Co-authored-by: AndIFellInTheRiver <AndIFellInTheRiver@users.noreply.github.com>

---
+++ src/Technology.js
@@ -0,0 +1,22 @@
+import produce from "immer";
+import { create } from "zustand";
+import "./App.css";
+import moneyStore from "./Game.js";
+
+/*
+export function Technology({ setTechOpen }) {
+  const money = moneyStore((state) => state.money);
+  return (
+    <>
+      <button onClick={}>ABB</button>
+      <button
+        className="techButton"
+        type="button"
+        onClick={() => setTechOpen(false)}
+      >
+        Close
+      </button>
+    </>
+  );
+}
+*/
