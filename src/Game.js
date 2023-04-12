70f8e66e01eb4f50ba7fc31d1ff24b76245531cb
IPPPPPI <17991307xiao@gmail.com>
2023-04-12 11:08:12 -0700

    save

--- src/Game.js
+++ src/Game.js
@@ -1,19 +1,49 @@
 import produce from "immer";
 import { create } from "zustand";
+import { useState } from "react";
 import "./App.css";
+import "./Technology.js";
+import { Technology } from "./Technology.js";
 
+/*
+const moneyStore = create((set) => ({
+  money: 500000,
+  population: 100000,
+  happiness: 100,
+  environment: 100,
+  populationchange: 0,
+  moneychange: 0,
+  environmentchange: 0,
+  happinesschange: 0,
+  year: 2000,
+}));
+*/
 //events for the games that will run down
-const money = 500000;
-const population = 100000;
-const happiness = 100;
-const environment = 100;
-const populationchange = 0;
-const moneychange = 0;
-const environmentchange = 0;
-const happinesschange = 0;
+
+let money = 500000;
+let population = 100000;
+let happiness = 100;
+let environment = 100;
+let populationchange = 0;
+let moneychange = 0;
+let environmentchange = 0;
+let happinesschange = 0;
 let year = 2000;
 
 export default function Game() {
+  /*const {
+    money,
+    population,
+    happiness,
+    environment,
+    moneychange,
+    happinesschange,
+    environmentchange,
+    populationchange,
+    year,
+  } = moneyStore();
+*/
+
   return (
     <>
       <div className="statbox">
@@ -33,6 +63,37 @@ export default function Game() {
           <p>Year: {year}</p>
         </div>
       </div>
+      {Clickables()}
     </>
+  );
+}
+
+function Clickables() {
+  const [techOpen, setTechOpen] = useState(false);
+  return (
+    <div>
+      {techOpen ? (
+        <>
+          <button>ABB</button>
+          <button
+            className="techButton"
+            type="button"
+            onClick={() => setTechOpen(false)}
+          >
+            Close
+          </button>
+        </>
+      ) : (
+        <>
+          <button
+            className="techButton"
+            type="button"
+            onClick={() => setTechOpen(true)}
+          >
+            Technology
+          </button>
+        </>
+      )}
+    </div>
   );
 }
